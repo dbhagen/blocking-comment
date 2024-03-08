2fc573cc31ea2f952c14f21ed0fa99c023bad1a3
Daniel Hagen <5039466+dbhagen@users.noreply.github.com>
2024-03-08 17:24:51 -0600

    wip: tweaks

---
+++ pre-commit-blocking-comment.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import re
+import sys
+
+
+def check_blocking_comments(files):
+    blocking_comments_found = False
+    for file_path in files:
+        with open(file_path, encoding='utf-8') as file:
+            content = file.read()
+            if (
+                # Java, JavaScript
+                re.search(
+                    r'^\s*//.*blocking-comment',
+                    content,
+                    re.MULTILINE,
+                ) or
+                # Java, JavaScript (multiline)
+                re.search(
+                    r'^\s*/\*.*blocking-comment',
+                    content,
+                    re.DOTALL,
+                ) or
+                # HTML
+                re.search(
+                    r'^\s*<!--.*blocking-comment',
+                    content,
+                    re.MULTILINE,
+                ) or
+                # HTML (multiline)
+                re.search(
+                    r'^\s*<!--.*blocking-comment',
+                    content,
+                    re.DOTALL,
+                ) or
+                # Markdown
+                re.search(
+                    r'^\s*<!--.*blocking-comment',
+                    content,
+                    re.MULTILINE,
+                ) or
+                re.search(
+                    r'^\s*<!--.*blocking-comment',
+                    content,
+                    re.DOTALL,
+                )       # Markdown (multiline)
+            ):
+                print(f"Blocking comment found in file: {file_path}")
+                print('Content:')
+                print(content)
+                blocking_comments_found = True
+    return blocking_comments_found
+
+
+if __name__ == '__main__':
+    files = sys.argv[1:]
+    if not check_blocking_comments(files):
+        sys.exit(0)  # No blocking comments found, allow the commit
+    else:
+        sys.exit(1)  # Blocking comments found, block the commit
