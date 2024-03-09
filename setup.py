93294eeea9e9ddf9051926fc7ac5eb02fbe63052
Daniel Hagen <5039466+dbhagen@users.noreply.github.com>
2024-03-08 18:12:37 -0600

    wip: tweaks

---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+from __future__ import annotations
+
+import pathlib
+
+from setuptools import setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='pre-commit-blocking-comment',
+    version='1.0.3',
+    description='Pre-commit hook to check for blocking comments before commit',
+    author='Daniel Hagen',
+    author_email='<5039466+dbhagen@users.noreply.github.com>',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dbhagen/pre-commit-blocking-comment',
+    python_requires='>=3.7, <4',
+    entry_points={
+        'console_scripts': [
+            'precommitblockingcomment=precommitblockingcomment:main',
+        ],
+    },
+    scripts=[
+        'precommitblockingcomment.py',
+    ],
+)
